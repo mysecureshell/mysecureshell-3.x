1dcd13a26128778f37bf1d1d4b2796c4820ecc0e
Teka101 <Teka101@github.com>
2015-02-08 14:17:08 +0100

    Code cleaning.

--- server/Main.c
+++ server/Main.c
@@ -15,119 +15,6 @@
 #include <arpa/inet.h>
 #include "TcpCheck.h"
 
-static void finish_this_one(int searchUid, unsigned long searchInode)
-{
-	char line[LINE_MAX];
-	int procfdlen, lnamelen;
-	char lname[30], cmdlbuf[512];
-	long inode;
-	const char *cs;
-	DIR *dirproc = NULL, *dirfd = NULL;
-	struct dirent *direproc, *direfd;
-
-	cmdlbuf[sizeof(cmdlbuf) - 1] = '\0';
-	if (!(dirproc = opendir(PATH_PROC)))
-		return;
-	while (errno = 0, direproc = readdir(dirproc))
-	{
-		if (direproc->d_type != DT_DIR)
-			continue;
-		for (cs = direproc->d_name; *cs; cs++)
-			if (!isdigit(*cs))
-				break;
-		if (*cs)
-			continue;
-		procfdlen = snprintf(line, sizeof(line), PATH_PROC_X_FD, direproc->d_name);
-		if (procfdlen <= 0 || procfdlen >= sizeof(line) - 5)
-			continue;
-		dirfd = opendir(line);
-		if (dirfd == NULL)
-			continue;
-		line[procfdlen] = '/';
-		while ((direfd = readdir(dirfd)))
-		{
-			if (direfd->d_type != DT_LNK)
-				continue;
-			if (procfdlen + 1 + strlen(direfd->d_name) + 1 > sizeof(line))
-				continue;
-
-			memcpy(line + procfdlen - PATH_FD_SUFFl, PATH_FD_SUFF "/", PATH_FD_SUFFl + 1);
-			strcpy(line + procfdlen + 1, direfd->d_name);
-			lnamelen = readlink(line, lname, sizeof(lname) - 1);
-			lname[lnamelen] = '\0';
-
-			inode = -1;
-			if (sscanf(lname, "socket:[%ld]", &inode) == 0)
-				if (sscanf(lname, "[0000]:%ld", &inode) == 0)
-					continue;
-			printf("[%s]lname [%s] => inode[%li] // %li\n", line, lname, inode, searchInode);
-
-			if (inode == searchInode)
-			{
-				char	tmp[2018];
-
-				snprintf(tmp, sizeof(tmp), "/proc/%s/exe", direproc->d_name);
-				printf("Try to readfile: %s\n", tmp);
-				if (readlink(tmp, line, sizeof(line)) > 0)
-					printf("APP: %s\n", line);
-			}
-		}
-		closedir(dirfd);
-		dirfd = NULL;
-	}
-	if (dirproc != NULL)
-		closedir(dirproc);
-	if (dirfd != NULL)
-		closedir(dirfd);
-}
-
-static void tcp_do_one(int lnr, const char *line, int localClientPort)
-{
-	unsigned long rxq, txq, time_len, retr, inode;
-	int num, local_port, rem_port, d, state, uid, timer_run, timeout;
-	char rem_addr[128], local_addr[128], more[512];
-
-	if (lnr == 0)
-		return;
-
-	num = sscanf(line,
-					"%d: %64[0-9A-Fa-f]:%X %64[0-9A-Fa-f]:%X %X %lX:%lX %X:%lX %lX %d %d %ld %512s\n",
-					&d, local_addr, &local_port, rem_addr, &rem_port, &state,
-					&txq, &rxq, &timer_run, &time_len, &retr, &uid, &timeout,
-					&inode, more);
-
-	if (num < 11)
-	{
-		fprintf(stderr, "warning, got bogus tcp line.\n");
-		return;
-	}
-	if (local_port == localClientPort)
-	{
-		printf("-> %s\n", line);
-		finish_this_one(uid, inode);
-	}
-}
-
-static int tcp_do(int localClientPort)
-{
-	FILE *fh;
-
-	if ((fh = fopen("/proc/net/tcp", "r")) == NULL)
-		perror("Unable to open proc/tcp");
-	else
-	{
-		char line[1024];
-		int nbLines = 0;
-
-		while (fgets(line, sizeof(line), fh) != NULL)
-		{
-			tcp_do_one(nbLines, line, localClientPort);
-			nbLines++;
-		}
-	}
-	return 0;
-}
-
 int main(int argc, char *argv[])
 {
 	fd_set master;
@@ -157,11 +44,10 @@ int main(int argc, char *argv[])
 	printf("Server-setsockopt() is OK...\n");
 
 	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = INADDR_ANY;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 	serveraddr.sin_port = htons(PORT);
 	memset(&(serveraddr.sin_zero), '\0', 8);
-	if (bind(listener, (struct sockaddr *) &serveraddr, sizeof(serveraddr))
-			== -1)
+	if (bind(listener, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) == -1)
 	{
 		perror("Server-bind() error lol!");
 		exit(1);
@@ -192,11 +78,12 @@ int main(int argc, char *argv[])
 				{
 					/* handle new connections */
 					addrlen = sizeof(clientaddr);
-					if ((newfd = accept(listener,
-							(struct sockaddr *) &clientaddr, &addrlen)) == -1)
+					if ((newfd = accept(listener, (struct sockaddr *) &clientaddr, &addrlen)) == -1)
 						perror("Server-accept() error lol!");
 					else
 					{
+						uid_t	uidClient;
+
 						printf("Server-accept() is OK...\n");
 						FD_SET(newfd, &master);
 						if (newfd > fdmax)
@@ -204,8 +91,8 @@ int main(int argc, char *argv[])
 						printf("%s: New connection from %s:%d on socket %d\n",
 								argv[0], inet_ntoa(clientaddr.sin_addr),
 								ntohs(clientaddr.sin_port), newfd);
-						//tcp_do(ntohs(clientaddr.sin_port));
-						TcpCheckIfClientIsMSS(ntohs(clientaddr.sin_port));
+						printf(" -> isMSS: %d\n", TcpCheckIfClientIsMSS(ntohs(clientaddr.sin_port), &uidClient));
+						printf(" -> uid client: %d\n", uidClient);
 					}
 				}
 				else
